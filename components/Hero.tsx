d938a7a11ba3efdee9517a7eb5fdfcc9486be284
BIEH08 <isaacescamilla.h1998@gmail.com>
2024-07-19 00:41:35 -0600

    I've fixed some bugs and created Experience, Approach and Footer section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -13,15 +13,12 @@ const Hero = () => {
           fill="white"
         />
         <Spotlight
-          className="-top-10 left-full h-[80vh] w-[50vw]"
+          className="h-[80vh] w-[50vw] top-10 left-full"
           fill="purple"
         />
-        <Spotlight
-          className="-top-40 -left-10  h-[80vh] w-[50vw]"
-          fill="blue"
-        />
+        <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
       </div>
-      <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.03] bg-grid-black/[0.2] absolute flex items-center justify-center">
+      <div className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.03] bg-grid-black/[0.2] absolute top-0 left-0 flex items-center justify-center">
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
       </div>
 
--- components/ui/InfiniteMovingCards.tsx
+++ components/ui/InfiniteMovingCards.tsx
@@ -104,19 +104,17 @@ export const InfiniteMovingCards = ({
                 {item.quote}
               </span>
               <div className="relative z-20 mt-6 flex flex-row items-center">
-                <span className="flex flex-col gap-1">
-                  <div className="me-3">
-                    <img src="/profile.svg" alt="profile" />
-                  </div>
-                  <div className="flex flex-col gap-1">
-                    <span className=" text-xl leading-[1.6] text-white font-bold">
-                      {item.name}
-                    </span>
-                    <span className=" text-sm leading-[1.6] text-white-200 font-normal">
-                      {item.title}
-                    </span>
-                  </div>
-                </span>
+                <div className="me-3">
+                  <img src="/profile.svg" alt="profile" />
+                </div>
+                <div className="flex flex-col gap-1">
+                  <span className=" text-xl leading-[1.6] text-white font-bold">
+                    {item.name}
+                  </span>
+                  <span className=" text-sm leading-[1.6] text-white-200 font-normal">
+                    {item.title}
+                  </span>
+                </div>
               </div>
             </blockquote>
           </li>
