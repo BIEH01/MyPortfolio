1b1670f9ea3f7ace1f09ebefec9d3ea8ebba5771
BIEH08 <isaacescamilla.h1998@gmail.com>
2024-09-28 02:39:57 -0600

    Fixing a bug for a comma

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,28 +6,29 @@ import { ThemeProvider } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Bryan's Portfolio",
-  description: "Modern & Minimalist BIEH08 Portfolio",
-  name="google-site-verification" content="fTiRyfx2kTCkhAP6zpB9fd3kLnzz4b6bk6W9-YTWGds"
+	title: "Bryan's Portfolio",
+	description: "Modern & Minimalist BIEH08 Portfolio",
+	name = "google-site-verification",
+	content = "fTiRyfx2kTCkhAP6zpB9fd3kLnzz4b6bk6W9-YTWGds",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="dark"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className={inter.className}>
+				<ThemeProvider
+					attribute="class"
+					defaultTheme="dark"
+					enableSystem
+					disableTransitionOnChange
+				>
+					{children}
+				</ThemeProvider>
+			</body>
+		</html>
+	);
 }
