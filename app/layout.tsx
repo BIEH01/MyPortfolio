19cc3f629db05978b3f301f7750063e2f300f4a5
BIEH08 <isaacescamilla.h1998@gmail.com>
2024-09-28 02:45:08 -0600

    Adding metadata bellow the html tag on layout.tsx file

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,6 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
 	title: "Bryan's Portfolio",
 	description: "Modern & Minimalist BIEH08 Portfolio",
-	name = "google-site-verification",
-	content = "fTiRyfx2kTCkhAP6zpB9fd3kLnzz4b6bk6W9-YTWGds",
 };
 
 export default function RootLayout({
@@ -19,6 +17,10 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
+			<meta
+				name="google-site-verification"
+				content="fTiRyfx2kTCkhAP6zpB9fd3kLnzz4b6bk6W9-YTWGds"
+			/>
 			<body className={inter.className}>
 				<ThemeProvider
 					attribute="class"
